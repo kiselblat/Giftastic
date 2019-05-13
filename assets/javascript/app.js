98430e4908471eb5865d9a052df2ad52cabd3bf8
kiselblat <tsechrist@outlook.com>
2019-05-13 16:41:48 -0500

    button clicks return JSON object to console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,27 @@
 var topics = ["dog" , "cat" , "chinchilla"]
 
-var searchTerm = "dog";
-var searchLimit = 3;
-var apiKey = "YVCQv8D9tDcc1PDdVMwyO5E7KhnTiTq8";
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=" + searchLimit + "&offset=0&lang=en";
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function(response) {
-  console.log(response);
-});
+
+var searchLimit = 3;
+
+var getGifs = function() {
+  var apiKey = "YVCQv8D9tDcc1PDdVMwyO5E7KhnTiTq8";
+  var searchTerm = $(this).attr("value");
+  console.log(searchTerm);
+  
+  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=" + searchLimit + "&offset=0&lang=en";
+  console.log(queryURL);
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function(response) {
+    console.log(response);
+  });
+}
 
 // Makes buttons
-function makeButtons() {
+var makeButtons = function() {
   console.log(topics);
   $('#buttons-display').append("This should appear and disappear too fast to see");
   // Delete the contents of buttons-view before adding new buttons
@@ -22,12 +30,15 @@ function makeButtons() {
   for (var i = 0; i < topics.length; i++) {
     var button = $('<input type="submit"/>');
     button.attr("value" , topics[i]);
+    button.on('click' , getGifs);
     $("#buttons-display").append(button);
     console.log(button.attr('value'));
   }
 }
 
+
+
+// initial call to make buttons
 $(document).ready(function() {
-  
   makeButtons();
 })
