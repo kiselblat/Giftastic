0bde9e918a6914efb0801f154861bdd8ef2f6385
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2019-05-17 00:50:45 -0500

    mainly code drying and nerdy comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,94 +1,129 @@
-var topics = ["computer" , "artificial intelligence" , "internet" , "robot" , "mainframe" , "hacker" , "machine" , "calculations"];
+var topics = ["computer" , "artificial intelligence" , "internet" , "robot" , "mainframe" , "hacker" , "machine" , "calculations" , "android" , "supercomputer" , "terminal"];
 var searchLimit = 10;
 
-// Makes buttons
-var makeButtons = function() {
-  console.log(topics);
-  $('#buttons-display').append("This should appear and disappear too fast to see");
+// Displays array in the form of buttons
+// Uses the global array topics
+var displayButtons = function() {
+  console.log("displayButtons(): ", topics);
   // Delete the contents of buttons-view before adding new buttons
   $('#buttons-display').empty();
   // Loop through the array, then generate buttons for each entry
   for (var i = 0; i < topics.length; i++) {
-    var button = $('<input type="submit"/>');
-    button.addClass('btn btn-outline-primary topic-button');
-    button.attr("value" , topics[i]);
-    button.on('click' , getGifs);
-    $("#buttons-display").append(button);
-    console.log(button.attr('value'));
+    $('#buttons-display').append(makeButton(topics[i]));
   }
+}
+
+// Makes a button
+// Takes an entry from the topics array through displayButtons
+// Returns a button
+var makeButton = function(topic) {
+  var button = $('<input type="submit"/>');
+  button
+    .addClass('btn btn-outline-primary topic-button')
+    .attr("value" , topic)
+    .on('click' , getGifs);
+  console.log(button.attr('value'));
+  return button;
 }
 
 // Adds button
+// Takes a string from the page text input
 var addButton = function(topic) {
   topics.push(topic);
-  console.log(topic , topics);
-  makeButtons();
+  console.log(topic);
+  displayButtons();
 }
  
-// Gets gifs
+// Searches GIPHY for gifs
 var getGifs = function() {
   var apiKey = "YVCQv8D9tDcc1PDdVMwyO5E7KhnTiTq8";
   var searchTerm = $(this).attr("value");
-  console.log(searchTerm);
-  
   var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + searchTerm + "&limit=" + searchLimit + "&offset=0&lang=en";
-  console.log(queryURL);
-
+  console.log(searchTerm , queryURL);
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(makeGifs);
+  }).then(displayGifs);
 };
 
-// Makes gifs
-var makeGifs = function(reply) {
+// Displays gifs on the site
+// Takes in a JSON object from GIPHY
+var displayGifs = function(reply) {
   var results = reply.data;
   console.log(results);
   for (var i = 0; i < results.length; i++) {
-    // make divs to put search results in
-    var searchResult = $('<span>');
-    var gif = $('<img>');
-    var gifRating = $('<p>');
-    // add attributes to the gif div
-    gif.attr('class' , 'clickable-gif');
-    gif.attr('data-moving' , results[i].images.fixed_height.url);
-    gif.attr('data-still' , results[i].images.fixed_height_still.url);
-    gif.attr('data-status' , 'still');
-    gif.attr('src' , results[i].images.fixed_height_still.url);
-    gif.on('click' , movingToggle);
-    // add rating to the gifRating div
-    gifRating.text("Rated: " + results[i].rating);
-    // add gif anf gifRating divs to the searchResult div...
-    searchResult.append(gif , gifRating);
-    searchResult.addClass('search-result');
-    // ...and add the search result to the site
-    $('#gif-display').prepend(searchResult);
+    $('#gif-display').prepend(makeResult(results[i]));
   }
+}
+
+// Packages each result into a div for display
+// Takes in an object from the list for GIPHY results
+// Returns a dynamically created searchResult div
+var makeResult = function(result) {
+  // grab info from the result
+  var moving = result.images.fixed_height.url;
+  var still = result.images.fixed_height_still.url;
+  var rating = result.rating;
+  // make divs to put info in and display
+  var searchResult = $('<span/>');
+  var gif = $('<img>');
+  var gifRating = $('<p/>');
+  // add attributes and click handler to the gif div
+  gif.addClass('clickable-gif')
+    .attr({
+      'data-moving' : moving,
+      'data-still' : still,
+      'data-status' : 'still',
+      'src' : still
+    })
+    .click(movingToggle);
+  // add rating to the gifRating div
+  gifRating.text("Rated: " + rating);
+  // add gif and gifRating divs to the searchResult div
+  searchResult
+    .append(gif , gifRating)
+    .addClass('search-result');
+  // return the search result
+  return searchResult;
 }
 
 // Toggles animation
 var movingToggle = function() {
-    var still = $(this).attr('data-still');
-    var moving = $(this).attr('data-moving');
-    var status = $(this).attr('data-status');
+  var still = $(this).attr('data-still');
+  var moving = $(this).attr('data-moving');
+  var status = $(this).attr('data-status');
+  // if the image is still make it moving...
   if (status === "still") {
-    $(this).attr('src' , moving);
-    $(this).attr("data-status" , "moving");
-  } else if (status === "moving") {
-    $(this).attr('src' , still);
-    $(this).attr("data-status" , "still");
+    $(this)
+      .attr({
+        'src' : moving,
+        'data-status' : 'moving'
+      });
+  // ...and vice versa
+  } else if (status === 'moving') {
+    $(this)
+      .attr({
+        'src' : still,
+        'data-status' : 'still'
+      });
+  // this should never take place but I'm paranoid
   } else {
     console.log("whoops!");
     return;
   }
 }
 
+// Wait until the page has loaded
 $(document).ready(function() {
-
+  // assign a click handler to the '+' button
   $('#add-new-topic').click(function (event) {
+    // allows use of the enter key for submit
     event.preventDefault();
-    addButton($('#new-topic').val().trim());
+    // get the new topic from the text input
+    var newTopic = $('#new-topic').val().trim();
+    // add the new topic to buttons
+    addButton(newTopic);
   });
   // initial call to make buttons
-  makeButtons();
+  displayButtons();
 })
